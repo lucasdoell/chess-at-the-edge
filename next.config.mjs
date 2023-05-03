1960f3c28aacca88d75e0b4cb06618d74e33ec12
Lucas Doell <lmd248@cornell.edu>
2023-05-03 01:57:43 -0400

    update next config for external images

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,16 @@ import "./env/env.mjs";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/u/55987186",
+      },
+    ],
+  },
   experimental: {
     appDir: true,
   },
