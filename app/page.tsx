0aa2840d67e9e9c0143d2a453b65a614e30cb264
Lucas Doell <lmd248@cornell.edu>
2023-05-03 02:08:57 -0400

    test commit

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className="h-screen container">
-      <p>Hello world</p>
+      <p>Hello world!</p>
     </main>
   );
 }
