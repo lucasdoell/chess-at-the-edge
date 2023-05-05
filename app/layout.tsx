21d91d18b30f6b1bbedb64e40e55ebfa41908e28
Lucas Doell <lmd248@cornell.edu>
2023-05-04 20:14:37 -0400

    add toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Analytics } from "@vercel/analytics/react";
 import { ThemeProvider } from "@/components/theme-provider";
 import { SiteHeader } from "@/components/site-header";
 import { SiteFooter } from "@/components/site-footer";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -41,11 +42,14 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className}`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <SiteHeader />
-          {children}
-          <Analytics />
-          <SiteFooter />
+          <div className="relative flex min-h-screen flex-col">
+            <SiteHeader />
+            <div className="flex-1">{children}</div>
+            <SiteFooter />
+          </div>
         </ThemeProvider>
+        <Analytics />
+        <Toaster />
       </body>
     </html>
   );
